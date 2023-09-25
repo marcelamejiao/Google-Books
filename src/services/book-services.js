3a7ff08ca51c9bcef74c7c61177bc393048d55a4
Marcela Mejia <mejiao24@hotmail.com>
2023-09-25 21:01:49 +1000

    include search parameters, erros and error styling

--- src/services/book-services.js
+++ src/services/book-services.js
@@ -9,8 +9,9 @@ export const getBooksData = async (searchTerms) => {
 	if (googleApiKey) {
 		authenticationKey = `&key=${googleApiKey}`
 	}
-	
-	const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodedSearchTerms}${authenticationKey}`, {
+
+	// Include inauthor and intitle in the query parameter to make the search more specific
+	const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${encodedSearchTerms}+intitle:${encodedSearchTerms}${authenticationKey}`, {
 		headers: {
 			Accept: 'application/json',
 		}
@@ -20,5 +21,11 @@ export const getBooksData = async (searchTerms) => {
     throw new Error('Failed to fetch this book');
   }
   const data = await response.json();
+
+	// Throw an error when there are no books in the response
+	if (!data.items) {
+		throw new Error('There are no books for this search.');
+	}
+
   return data.items;
 }
