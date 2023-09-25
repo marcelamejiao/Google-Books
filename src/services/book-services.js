6e596d76b1ca1e1d4f6110262045c0a563f34d81
Marcela Mejia <mejiao24@hotmail.com>
2023-09-25 20:37:10 +1000

    update book service to load google api key from local storage

--- src/services/book-services.js
+++ src/services/book-services.js
@@ -1,9 +1,16 @@
 export const getBooksData = async (searchTerms) => {
 	
 	// Protect the URL from users inputs by using special characters that belongs to the URL structure
-	const encodedSearchTerms = encodeURIComponent(searchTerms)
+	const encodedSearchTerms = encodeURIComponent(searchTerms);
+
+	// Load the API Key from local storage if we have one
+	const googleApiKey = window.localStorage.getItem('GOOGLE_API_KEY');
+	let authenticationKey = '';
+	if (googleApiKey) {
+		authenticationKey = `&key=${googleApiKey}`
+	}
 	
-	const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodedSearchTerms}`, {
+	const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodedSearchTerms}${authenticationKey}`, {
 		headers: {
 			Accept: 'application/json',
 		}
